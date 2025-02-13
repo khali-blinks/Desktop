688a0f6e93304f6f12ed9da52619b2d4192c91e9
Khalifa <kismaiel61@gmail.com>
2025-02-13 18:42:14 -0500

    Final

--- Modern React and Redux/pics/src/App.js
+++ Modern React and Redux/pics/src/App.js
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import SearchBar from "./components/SearchBar";
+import ImageList from "./components/ImageList";
 import searchImages from "./api";
 
 function App(){
-    const handleSubmit = (term) => {
-        searchImages(term);
+    const [images,setImages] = useState([]);
+
+    const handleSubmit = async(term) => {
+        const results = await searchImages(term);
+        setImages(results);
     }
     return (
         <div>
             <SearchBar onSubmit={handleSubmit}/>
+            <ImageList images={images}/>
         </div>
     );
 }
