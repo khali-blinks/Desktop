f4dfddd22e3eebc3fd232bac1589b4ed53953a20
Khalifa <kismaiel61@gmail.com>
2025-02-22 16:34:48 -0500

    state

--- Modern React and Redux/comps/src/components/Accordion.js
+++ Modern React and Redux/comps/src/components/Accordion.js
@@ -2,11 +2,13 @@ import { useState } from "react";
 
 function Accordion({items}){
     const [expandedIndex, setExpandedIndex] = useState(0);
-    const renderedItems = items.map((item) => {
+
+    const renderedItems = items.map((item,index) => {
+        const isExpanded = index === expandedIndex;
         return (
             <div key={item.id}>
                 <div>{item.label}</div>
-                <div>{item.content}</div>
+                {isExpanded && <div>{item.content}</div>}
             </div>
         )
     })
