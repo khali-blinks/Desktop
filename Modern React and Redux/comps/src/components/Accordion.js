2d0f500f8af3b803845ed5f2dca1d90376665e66
Khalifa <kismaiel61@gmail.com>
2025-02-23 10:33:51 -0500

    flex

--- Modern React and Redux/comps/src/components/Accordion.js
+++ Modern React and Redux/comps/src/components/Accordion.js
@@ -7,15 +7,19 @@ function Accordion({items}){
     const renderedItems = items.map((item,index) => {
         const isExpanded = index === expandedIndex;
 
-        const icon = <span>{isExpanded ? <GoChevronDown/> : <GoChevronLeft/>}</span>
+        const icon = <span className="text-2xl">{isExpanded ? <GoChevronDown/> : <GoChevronLeft/>}</span>
         return (
             <div key={item.id}>
-                <div onClick={()=> setExpandedIndex(index)}>{icon} {item.label}</div>
-                {isExpanded && <div>{item.content}</div>}
+                <div className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+                onClick={()=> setExpandedIndex(index)}>
+                    {item.label}
+                    {icon} 
+                </div>
+                {isExpanded && <div className="border-b p-5">{item.content}</div>}
             </div>
         )
     })
-    return <div>{renderedItems}</div>;
+    return <div className="border-x border-t rounded">{renderedItems}</div>;
 }
 
 export default Accordion;
