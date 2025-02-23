0f8417f3d48e67d8133a1d98d563092362e462bb
Khalifa <kismaiel61@gmail.com>
2025-02-23 10:25:52 -0500

    expanded

--- Modern React and Redux/comps/src/components/Accordion.js
+++ Modern React and Redux/comps/src/components/Accordion.js
@@ -1,13 +1,16 @@
 import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({items}){
     const [expandedIndex, setExpandedIndex] = useState(1);
 
     const renderedItems = items.map((item,index) => {
         const isExpanded = index === expandedIndex;
+
+        const icon = <span>{isExpanded ? <GoChevronDown/> : <GoChevronLeft/>}</span>
         return (
             <div key={item.id}>
-                <div onClick={()=> setExpandedIndex(index)}>{item.label}</div>
+                <div onClick={()=> setExpandedIndex(index)}>{icon} {item.label}</div>
                 {isExpanded && <div>{item.content}</div>}
             </div>
         )
