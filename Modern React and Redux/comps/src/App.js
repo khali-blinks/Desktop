3a8e0cf422f88a238b0b977cff48fbfb4bf18b09
Khalifa <kismaiel61@gmail.com>
2025-02-25 18:42:47 -0500

    convention

--- Modern React and Redux/comps/src/App.js
+++ Modern React and Redux/comps/src/App.js
@@ -1,12 +1,19 @@
 import Dropdown from "./components/DropDown"
+import { useState } from "react";
 
 function App() {
+    const [selection, setSelection] = useState(null);
+
+    const handleSelect = (option) => {
+        setSelection(option);
+    }
+
     const options = [
         { label : 'Red', value: 'red'},
         { label : 'Green', value: 'green'},
         { label : 'Blue', value: 'blue'}
     ]
-    return <Dropdown options={options}/>
+    return <Dropdown options={options} value={selection} onChange={handleSelect}/>
 }
 
 export default App;
