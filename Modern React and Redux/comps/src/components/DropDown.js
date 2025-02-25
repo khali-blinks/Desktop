a757b89057ca43760a43c0a51b2ec965eedeb8dd
Khalifa <kismaiel61@gmail.com>
2025-02-24 20:34:05 -0500

    Drop

---
+++ Modern React and Redux/comps/src/components/DropDown.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function Dropdown({options}){
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    }
+
+    const handleOptionClick = () => {
+        setIsOpen(false);
+    }
+
+    const renderedOptions = options.map((option) => {
+        return <div onClick={handleOptionClick} key={option.value}>{option.label}</div>
+    })
+    return (
+        <div>
+            <div onClick={handleClick}>Select ...</div>
+            {isOpen && <div>{renderedOptions}</div>}
+        </div>
+    )
+}
+
+export default Dropdown;
