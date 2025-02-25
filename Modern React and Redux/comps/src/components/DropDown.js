f00f9cebc8e1bcf337ebfee9c7c0bfbf77bee724
Khalifa <kismaiel61@gmail.com>
2025-02-25 18:28:30 -0500

    helper

--- Modern React and Redux/comps/src/components/DropDown.js
+++ Modern React and Redux/comps/src/components/DropDown.js
@@ -1,22 +1,24 @@
 import { useState } from "react";
 
-function Dropdown({options}){
+function Dropdown({options, onSelect, selection}){
     const [isOpen, setIsOpen] = useState(false);
 
     const handleClick = () => {
         setIsOpen(!isOpen);
     }
 
-    const handleOptionClick = () => {
+    const handleOptionClick = (option) => {
         setIsOpen(false);
+        onSelect(option);
     }
 
     const renderedOptions = options.map((option) => {
-        return <div onClick={handleOptionClick} key={option.value}>{option.label}</div>
+        return <div onClick={() => handleOptionClick(option)} key={option.value}>{option.label}</div>
     })
+
     return (
         <div>
-            <div onClick={handleClick}>Select ...</div>
+            <div onClick={handleClick}>{selection?.label || 'Select...'}</div>
             {isOpen && <div>{renderedOptions}</div>}
         </div>
     )
