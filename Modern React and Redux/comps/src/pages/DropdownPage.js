f783d0132488f9e575c26abea4e07a2d0d409476
Khalifa <kismaiel61@gmail.com>
2025-03-02 16:56:45 -0500

    dodsp

--- Modern React and Redux/comps/src/pages/DropdownPage.js
+++ Modern React and Redux/comps/src/pages/DropdownPage.js
@@ -21,3 +21,13 @@ export default DropdownPage;
 function App() {
     return <div></div>
 }
+import classNames from "classnames";
+
+function Panel({children, className, ...rest}) {
+    const finalClassNames = classNames('border rounded p-3 shadow bg-white w-full', className);
+
+    return <div {...rest} className={finalClassNames}>{children}</div>
+
+}
+
+export default Panel;
