8a04a17a9114e4277793da55bbc3a6975089e6f8
Khalifa <kismaiel61@gmail.com>
2025-03-02 15:30:32 -0500

    drop

---
+++ Modern React and Redux/comps/src/pages/DropdownPage.js
@@ -0,0 +1,19 @@
+import Dropdown from "../components/DropDown";
+import { useState } from "react";
+
+function DropdownPage() {
+    const [selection, setSelection] = useState(null);
+
+    const handleSelect = (option) => {
+        setSelection(option);
+    }
+
+    const options = [
+        { label : 'Red', value: 'red'},
+        { label : 'Green', value: 'green'},
+        { label : 'Blue', value: 'blue'}
+    ]
+    return <Dropdown options={options} value={selection} onChange={handleSelect}/>
+}
+
+export default DropdownPage;
