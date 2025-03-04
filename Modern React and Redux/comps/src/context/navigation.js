66af30c20a007d17707337f7e9dac701514a1efe
Khalifa <kismaiel61@gmail.com>
2025-03-03 21:24:15 -0500

    window

--- Modern React and Redux/comps/src/context/navigation.js
+++ Modern React and Redux/comps/src/context/navigation.js
@@ -1,9 +1,22 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 
 const NavigationContext = createContext();
 
 function NavigationProvider(children){
     const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+    useEffect(() => {
+        const handler = () => {
+            setCurrentPath(window.location.pathname);
+        };
+        window.addEventListener('popstate', handler);
+
+        return () => {
+            window.removeEventListener('popstate', handler);
+        }
+
+    },[]);
+
     return (
         <NavigationContext.Provider value={{}}>
             {children}
