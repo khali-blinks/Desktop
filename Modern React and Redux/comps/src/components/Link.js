eefa153b57e7cf3945fe79c22e6ee3c7cbe53435
Khalifa <kismaiel61@gmail.com>
2025-03-05 20:03:31 -0500

    link

--- Modern React and Redux/comps/src/components/Link.js
+++ Modern React and Redux/comps/src/components/Link.js
@@ -1,9 +1,16 @@
-import { Children } from "react";
+import { useContext } from "react";
+import NavigationContext from "../context/navigation";
 
-function Link(){
-    const handleClick = () => {
+function Link({to, children}){
+    const { navigate } = useContext(NavigationContext);
 
+    const handleClick = (event) => {
+        event.preventDefault();
+
+        navigate(to);
     };
 
-    return <a onClick={handleClick}>{Children}</a>
+    return <a onClick={handleClick}>{children}</a>
 }
+
+export default Link;
