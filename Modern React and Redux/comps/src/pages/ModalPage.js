fb435d961ec2637cdd0b94f636bba6f3792f99b7
Khalifa <kismaiel61@gmail.com>
2025-03-08 10:56:29 -0500

    customize

---
+++ Modern React and Redux/comps/src/pages/ModalPage.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import Modal from '../components/Modal';
+import Button from '../components/Button';
+
+function ModalPage(){
+    const [showModal, setShowModal] = useState(false);
+
+    const handleClick = () => {
+        setShowModal(true);
+    };
+
+    const handleClose = () => {
+        setShowModal(false);
+    }
+
+    return (
+        <div>
+            <Button onClick={handleClick} primary>Open Modal</Button>
+            {showModal && <Modal onClose={handleClose}/>}
+        </div>
+    );
+};
+
+export default ModalPage;
