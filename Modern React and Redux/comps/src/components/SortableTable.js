b8dfbb237720ddb0b7fc975e9e82a17ee2e987fa
Khalifa <kismaiel61@gmail.com>
2025-03-13 18:23:19 -0400

    sort

--- Modern React and Redux/comps/src/components/SortableTable.js
+++ Modern React and Redux/comps/src/components/SortableTable.js
@@ -1,7 +1,23 @@
 import Table from "./Table";
 
 function SortableTable(props){
-    return <Table {...props}/>
+    const { config } = props;
+
+    const handleClick = (label) => {
+        console.log(label)
+    }
+
+    const updatedConfig = config.map((column) => {
+        if (!column.sortValue){
+            return column
+        }
+
+        return {
+            ...column,
+            header : () => <th onClick={()=> handleClick(column.label)}>{column.label} is sort</th>
+        }
+    })
+    return <Table {...props} config={updatedConfig}/>
 }
 
 export default SortableTable;
