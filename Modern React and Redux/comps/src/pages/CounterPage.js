961f3f7260c09e2eb89f842b7f75cb1434e03f68
Khalifa <kismaiel61@gmail.com>
2025-03-17 21:20:17 -0400

    redux

---
+++ Modern React and Redux/comps/src/pages/CounterPage.js
@@ -0,0 +1,58 @@
+import Button from "../components/Button";
+import Panel from "../components/Panel";
+import { useReducer } from "react";
+
+const reducer = (state,action) => {
+    //
+}
+
+function CounterPage({ initialCount}){
+    //const [count, setCount] = useState(initialCount);
+    //const [valueToAdd, setValueToAdd] = useState(0);
+    const [state,dispatch] = useReducer(reducer, {
+        count:initialCount,
+        valueToAdd:0
+    })
+
+    const increment = () => {
+        //setCount(count + 1);
+    }
+
+    const decrement = () => {
+        //setCount(count - 1);
+    }
+
+    const handleChange = (event) => {
+        const value = parseInt(event.target.value) || 0;
+        //setValueToAdd(value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        //setCount(count + valueToAdd);
+        //setValueToAdd(0);
+    }
+
+
+    return (
+        <Panel className="m-3">
+            <h1 className="text-lg">Count is {count}</h1>
+            <div className="flex flex-row">
+                <Button onClick={increment}>Increment</Button>
+                <Button onClick={decrement}>Decrement</Button>
+            </div>
+
+            <form onSubmit={handleSubmit}>
+                <label>Add a lot!</label>
+                <input value={valueToAdd || ''} 
+                    onChange={handleChange} 
+                    type="number" 
+                    className="p-1 m-3 bg-gray-50 border border-gray-300">
+                </input>
+                <Button>Add it!</Button>
+            </form>
+        </Panel>)
+}
+
+export default CounterPage;
+
