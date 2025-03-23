7036181e6e4337ae72e52cf8d1a166d6d2cf1c6d
Khalifa <kismaiel61@gmail.com>
2025-03-23 12:18:22 -0400

    store

--- Modern React and Redux/comps/src/pages/CounterPage.js
+++ Modern React and Redux/comps/src/pages/CounterPage.js
@@ -89,4 +89,17 @@ export default CounterPage;
 
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
+const songsSlice = createSlice({
+    name: "song",
+    initialState: [],
+    reducers: {
+      addSong(state, action) {
+        state.push(action.payload);
+      },
+      removeSong(state, action) {
+        //
+      },
+    },
+  });
+
 
