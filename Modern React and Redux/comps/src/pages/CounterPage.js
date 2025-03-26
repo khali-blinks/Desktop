09d229930dd4370b2baed72435568752ef41f0da
Khalifa <kismaiel61@gmail.com>
2025-03-25 21:26:13 -0400

    remove

--- Modern React and Redux/comps/src/pages/CounterPage.js
+++ Modern React and Redux/comps/src/pages/CounterPage.js
@@ -87,26 +87,7 @@ function CounterPage({ initialCount}){
 
 export default CounterPage;
 
-import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const songsSlice = createSlice({
-  name: "song",
-  initialState: [],
-  reducers: {
-    addSong(state, action) {
-      state.push(action.payload);
-    },
-    removeSong(state, action) {
-      const index = state.indexOf(action.payload);
-      state.splice(index, 1);
-    },
-  },
-});
-const store = configureStore({
-    reducer: {
-      songs: songsSlice.reducer,
-    },
-  });
 
 
 
