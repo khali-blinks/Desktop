fd16d303c8323ca1e32926c613534bcbcf5a8f88
Khalifa <kismaiel61@gmail.com>
2025-03-27 21:41:20 -0400

    reke

--- Modern React and Redux/comps/src/pages/CounterPage.js
+++ Modern React and Redux/comps/src/pages/CounterPage.js
@@ -87,43 +87,7 @@ function CounterPage({ initialCount}){
 
 export default CounterPage;
 
-import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const moviesSlice = createSlice({
-  name: "movie",
-  initialState: [],
-  reducers: {
-    addMovie(state, action) {
-      state.push(action.payload);
-    },
-    removeMovie(state, action) {
-      const index = state.indexOf(action.payload);
-      state.splice(index, 1);
-    },
-    reset(state, action) {
-      return [];
-    },
-  },
-});
-
-const songsSlice = createSlice({
-    name: "song",
-    initialState: [],
-    reducers: {
-      addSong(state, action) {
-        state.push(action.payload);
-      },
-      removeSong(state, action) {
-        const index = state.indexOf(action.payload);
-        state.splice(index, 1);
-      },
-    },
-    extraReducers(builder) {
-      builder.addCase(moviesSlice.actions.reset, (state, action) => {
-        return [];
-      });
-    },
-  });
 
 
 
