01e6ba54789d7d29416cb254c74b5e4e15f7b41e
Khalifa <kismaiel61@gmail.com>
2025-03-28 22:48:07 -0400

    re

--- Modern React and Redux/comps/src/pages/CounterPage.js
+++ Modern React and Redux/comps/src/pages/CounterPage.js
@@ -87,7 +87,24 @@ function CounterPage({ initialCount}){
 
 export default CounterPage;
 
-
+const songsSlice = createSlice({
+    name: "song",
+    initialState: [],
+    reducers: {
+      addSong(state, action) {
+        state.push(action.payload);
+      },
+      removeSong(state, action) {
+        const index = state.indexOf(action.payload);
+        state.splice(index, 1);
+      },
+    },
+    extraReducers(builder) {
+      builder.addCase(moviesSlice.actions.reset, (state, action) => {
+        return [];
+      });
+    },
+  });
 
 
 
