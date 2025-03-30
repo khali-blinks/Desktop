a9ad5ff94c73f2785589b1f8f3c0e309628ae601
Khalifa <kismaiel61@gmail.com>
2025-03-29 21:53:17 -0400

    Andor

--- Modern React and Redux/comps/src/pages/CounterPage.js
+++ Modern React and Redux/comps/src/pages/CounterPage.js
@@ -106,3 +106,21 @@ const songsSlice = createSlice({
       });
     },
   });
+  const songsSlice = createSlice({
+    name: "song",
+    initialState: [],
+    reducers: {
+      addSong(state, action) {
+        state.push(action.payload);
+      },
+      removeSong(state, action) {
+        const index = state.indexOf(action.payload);
+        state.splice(index, 1);
+      },
+    },
+    extraReducers(builder) {
+      builder.addCase(moviesSlice.actions.reset, (state, action) => {
+        return [];
+      });
+    },
+  });
