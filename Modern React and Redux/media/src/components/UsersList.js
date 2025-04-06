3b14a1aaff28aa3f43eadc3bd76f2df0a9660d6d
Khalifa <kismaiel61@gmail.com>
2025-04-06 10:22:14 -0400

    UPDATE

---
+++ Modern React and Redux/media/src/components/UsersList.js
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../store";
+
+function UsersList(){
+    return 'Users List';
+}
+
+export default UsersList;
