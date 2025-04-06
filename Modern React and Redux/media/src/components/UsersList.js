d235db5914f772d6b210f27b9a613524cbd25359
Khalifa <kismaiel61@gmail.com>
2025-04-06 10:32:29 -0400

    state

--- Modern React and Redux/media/src/components/UsersList.js
+++ Modern React and Redux/media/src/components/UsersList.js
@@ -1,8 +1,18 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
 
 function UsersList(){
+    const dispatch = useDispatch();
+    const {isLoading, data, error } = useSelector((state) => {
+
+    })
+
+
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, [dispatch])
+
     return 'Users List';
 }
 
