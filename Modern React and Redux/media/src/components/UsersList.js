1ea8bccbd7957f91bd1f737774a1b94be1733cfd
Khalifa <kismaiel61@gmail.com>
2025-04-08 20:19:39 -0400

    name

--- Modern React and Redux/media/src/components/UsersList.js
+++ Modern React and Redux/media/src/components/UsersList.js
@@ -23,6 +23,9 @@ function UsersList(){
 
     const renderedUsers = data.map((user) => {
         return <div key={user.id} className="mb-2 border rounded">
+            <div className="flex p-2 justify-between items-center cursor-pointer">
+                {user.name}
+            </div>
 
         </div>
     })
