6c82440e842caa600f92267727b25bf717b13c8a
Khalifa <kismaiel61@gmail.com>
2025-04-13 10:33:05 -0400

    downloaded dep

--- Modern React and Redux/media/tailwind.config.js
+++ Modern React and Redux/media/tailwind.config.js
@@ -1,21 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       keyframes: {
-        shimmer:{
-          '100%':{ transform: 'translateX(100%)'}
+        shimmer: {
+          '100%': { transform: 'translateX(100%)' },
         },
       },
       animation: {
-        shimmer: 'shimmer 1.5s infinite'
-      }
+        shimmer: 'shimmer 1.5s infinite',
+      },
     },
   },
   plugins: [],
-}
-
+};
