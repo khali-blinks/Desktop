9d97cc0f72290eb06eec60fca0d4064d4c0fe562
Khalifa <kismaiel61@gmail.com>
2025-04-14 11:07:20 -0400

    photos

--- Modern React and Redux/media/src/components/AlbumsList.js
+++ Modern React and Redux/media/src/components/AlbumsList.js
@@ -1,8 +1,23 @@
 import { useFetchAlbumsQuery } from "../store";
+import Skeleton from './Skeleton';
+import ExpandablePanel from "./ExpandablePanel";
+import Button from './Button';
 
 function AlbumsList({ user }){
     const { data, error, isLoading } = useFetchAlbumsQuery(user);
-    console.log(data,error,isLoading)
+    
+    let content;
+    if(isLoading){
+        content = <Skeleton times={3}/>
+    } else if(error){
+        content = <div>Error loading albums.</div>
+    } else {
+        content = data.map(album => {
+            return <ExpandablePanel>
+                List of photos in the album
+            </ExpandablePanel>
+        })
+    }
     return (<div>Albums for {user.name}</div>)
 }
 
