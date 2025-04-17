2b73fa89d353a992dff97eeb68a2b3bdd209847f
Khalifa <kismaiel61@gmail.com>
2025-04-17 10:41:22 -0400

    phtoapi

---
+++ Modern React and Redux/media/src/store/apis/photosApi.js
@@ -0,0 +1,47 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { faker } from "@faker-js/faker";
+
+const photosApi = createApi({
+    reducerPath: 'photos',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:3005'
+    }),
+    endpoints(builder){
+        return {
+            fetchPhotos: builder.query({
+                query: (album) => {
+                    return {
+                        url: '/photos',
+                        params: {
+                            albumId: album.id
+                        },
+                        method: 'GET'
+                    }
+                }
+            }),
+            addPhoto: builder.mutation({
+                query: (album) => {
+                    return {
+                        method: 'POST',
+                        url: '/photos',
+                        body: {
+                            albumId: album.id,
+                            url: faker.image.abstract(150,150,true)
+                        }
+                    }
+                }
+            }),
+            removePhoto: builder.mutation({
+                query: (photo) => {
+                    return {
+                        method: 'DELETE',
+                        url: `/photos/${photo.id}`
+                    }
+                }
+            })
+        }
+    }
+})
+
+export { photosApi };
+export const { useFetchPhotosQuery, useAddPhotoMutation, useRemovePhotoMutation } = photosApi;
