ff8906f956c624ffd30993f86cdb35bf9f6d2782
Khalifa <kismaiel61@gmail.com>
2025-04-17 11:10:47 -0400

    mutation

--- Modern React and Redux/media/src/components/PhotosList.js
+++ Modern React and Redux/media/src/components/PhotosList.js
@@ -1,7 +1,22 @@
-
+import { useFetchPhotosQuery, useAddPhotoMutation } from "../store";
+import Button from './Button';
 
 function PhotosList({ album }){
-    return 'PhotosList'
+    useFetchPhotosQuery(album);
+    const [addPhoto, addPhotosResults] = useAddPhotoMutation(album);
+
+    const handleAddPhoto = () => {
+        addPhoto(album);
+    }
+
+    return (
+        <div>
+            <div className="m-2 flex flex-row items-center justify-between">
+                <h3 className="text-lg font-bold">Photos In {album.title}</h3>
+                <Button loading={addPhotosResults.isLoading} onClick={handleAddPhoto}>+ Add Photo</Button>
+            </div>
+        </div>
+    )
 }
 
 export default PhotosList;
