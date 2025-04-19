9c969d67c8ea212d5ef6569b24f80e473acee101
Khalifa <kismaiel61@gmail.com>
2025-04-19 15:41:34 -0400

    mutation1

--- Modern React and Redux/media/src/components/PhotosList.js
+++ Modern React and Redux/media/src/components/PhotosList.js
@@ -1,12 +1,25 @@
 import { useFetchPhotosQuery, useAddPhotoMutation } from "../store";
 import Button from './Button';
+import Skeleton from "./Skeleton";
+import PhotosListItem from "./PhotosListItem";
 
 function PhotosList({ album }){
-    useFetchPhotosQuery(album);
+    const {data, isFetching, error}= useFetchPhotosQuery(album);
     const [addPhoto, addPhotosResults] = useAddPhotoMutation(album);
 
     const handleAddPhoto = () => {
         addPhoto(album);
+    }
+
+    let content;
+    if (isFetching){
+        content = <Skeleton className="h-8 w-8" times={4}/>
+    } else if(error){
+        content = <div>Error fetching photos...</div>
+    } else {
+        content = data.map((photo) => {
+            return <PhotosListItem key={photo.id} photo={photo}/>
+        })
     }
 
     return (
@@ -14,6 +27,9 @@ function PhotosList({ album }){
             <div className="m-2 flex flex-row items-center justify-between">
                 <h3 className="text-lg font-bold">Photos In {album.title}</h3>
                 <Button loading={addPhotosResults.isLoading} onClick={handleAddPhoto}>+ Add Photo</Button>
+            </div>
+            <div className="mx-8 flex flex-row flex wrap justify-center">
+                {content}
             </div>
         </div>
     )
