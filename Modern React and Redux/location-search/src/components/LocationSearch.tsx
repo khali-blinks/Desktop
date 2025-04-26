307d90f6474dc652ffd28a0f540532c1d7437a8c
Khalifa <kismaiel61@gmail.com>
2025-04-26 10:33:48 -0400

    term state

--- Modern React and Redux/location-search/src/components/LocationSearch.tsx
+++ Modern React and Redux/location-search/src/components/LocationSearch.tsx
@@ -6,6 +6,20 @@ interface LocationSearchProps{
 }
 
 export default function LocationSearch({onPlaceClick}:LocationSearchProps){
-    const [places,setPlaces] = useState<Place[]>([])
-    return <div>Search for a location!</div>
+    const [places,setPlaces] = useState<Place[]>([]);
+    const [term,setTerm] = useState('');
+    
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label className="font-bold" htmlFor="term">Search</label>
+            </form>
+            <input 
+                className="border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full" 
+                id="term"
+                value={term}
+                onChange={e => setTerm(e.target.value)}
+            /> 
+        </div>
+        )
 }
