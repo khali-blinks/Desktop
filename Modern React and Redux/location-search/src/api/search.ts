282f09038d610e5ef1ca454b1806a3cbc333d3d0
Khalifa <kismaiel61@gmail.com>
2025-04-28 17:49:26 -0400

    mapping

--- Modern React and Redux/location-search/src/api/search.ts
+++ Modern React and Redux/location-search/src/api/search.ts
@@ -17,6 +17,14 @@ export const search = async (term: string) => {
         `https://nominatim.openstreetmap.org/search?q=${term}&format=geojson&addressdetails=1&layer=address&limit=5`
     );
     const data: SearchResponse = await res.json();
-    console.log(data);
+    
+    const places: Place[] = data.features.map((feature) => {
+        return{
+            id: feature.properties.place_id,
+            name: feature.properties.display_name,
+            longitude: feature.geometry.coordinates[0],
+            latitude: feature.geometry.coordinates[1]
+        }
+    })
 
 }
