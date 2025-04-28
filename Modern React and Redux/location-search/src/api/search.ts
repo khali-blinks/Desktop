20d73d88cf553f34a57efaf1852e929846f77fb7
Khalifa <kismaiel61@gmail.com>
2025-04-28 17:43:27 -0400

    data format

--- Modern React and Redux/location-search/src/api/search.ts
+++ Modern React and Redux/location-search/src/api/search.ts
@@ -1,7 +1,22 @@
+import type { Place } from "./Place";
+
+interface SearchResponse{
+    features:{
+        geometry:{
+            coordinates: number[];
+        }
+        properties:{
+            place_id: number;
+            display_name: string;
+        }
+    }[]
+}
+
 export const search = async (term: string) => {
     const res = await fetch(
         `https://nominatim.openstreetmap.org/search?q=${term}&format=geojson&addressdetails=1&layer=address&limit=5`
     );
-    const data = await res.json();
-    data.response
+    const data: SearchResponse = await res.json();
+    console.log(data);
+
 }
