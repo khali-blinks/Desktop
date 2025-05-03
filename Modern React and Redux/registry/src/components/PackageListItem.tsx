1b602192c8905ea6adfdf98f5000a5f75b6a438b
Khalifa <kismaiel61@gmail.com>
2025-05-03 10:12:40 -0400

    Update 1

--- Modern React and Redux/registry/src/components/PackageListItem.tsx
+++ Modern React and Redux/registry/src/components/PackageListItem.tsx
@@ -1,9 +1,16 @@
 import type { PackageSummary } from "../api/types/packageSummary";
+ import { Link } from 'react-router-dom'
 
 interface packageListItemProps{
-
+    pack: PackageSummary;
 }
 
 export default function PackageListItem({ pack }: PackageListItemProps){
-    
+    return(
+        <div className="border p-4 rounded flex justify-between items-center">
+            <div className="flex flex-col gap-2">
+                <Link to={`/packages/${pack.name}`}></Link>
+            </div>
+        </div>
+    )
 }
