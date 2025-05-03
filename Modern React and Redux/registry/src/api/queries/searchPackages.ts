5716455ac84dc636d80f692fe716de8fc5852b1c
Khalifa <kismaiel61@gmail.com>
2025-05-02 22:48:42 -0400

    map result

--- Modern React and Redux/registry/src/api/queries/searchPackages.ts
+++ Modern React and Redux/registry/src/api/queries/searchPackages.ts
@@ -21,7 +21,7 @@ export async function searchPackages(term:string): Promise<PackageSummary[]> {
                 name,
                 version,
                 description,
-                keywords
+                keywords,
             }
         })
 }
