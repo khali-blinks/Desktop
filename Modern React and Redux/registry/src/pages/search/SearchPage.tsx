3547000bac060f3e3045d494bee45da8ae644955
Khalifa <kismaiel61@gmail.com>
2025-05-03 09:55:53 -0400

    type

--- Modern React and Redux/registry/src/pages/search/SearchPage.tsx
+++ Modern React and Redux/registry/src/pages/search/SearchPage.tsx
@@ -3,6 +3,6 @@ import { useLoaderData } from "react-router-dom"
 
 export default function SearchPage(){
     const data = useLoaderData() as SearchLoaderResult;
-    console.log(data);
+    console.log(data.searchResults);
     return <div>Search Page</div>
 }
