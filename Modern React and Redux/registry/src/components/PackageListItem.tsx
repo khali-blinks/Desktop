27fc13f10d9dc7e086b396b808f4f6bbea4a8031
Khalifa <kismaiel61@gmail.com>
2025-05-03 10:04:57 -0400

    Initial Commit

---
+++ Modern React and Redux/registry/src/components/PackageListItem.tsx
@@ -0,0 +1,9 @@
+import type { PackageSummary } from "../api/types/packageSummary";
+
+interface packageListItemProps{
+
+}
+
+export default function PackageListItem({ pack }: PackageListItemProps){
+    
+}
