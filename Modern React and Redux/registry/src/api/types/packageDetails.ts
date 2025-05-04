4644c3d30f0cebef25ca48cfa169afcc9fc36227
Khalifa <kismaiel61@gmail.com>
2025-05-04 10:02:26 -0400

    typesetup

--- Modern React and Redux/registry/src/api/types/packageDetails.ts
+++ Modern React and Redux/registry/src/api/types/packageDetails.ts
@@ -1,3 +1,10 @@
 export interface PackageDetails{
-    
+    name: string;
+    description: string;
+    readme: string;
+    author: {
+        email: string;
+        name: string;
+    }
+    maintainers
 }
