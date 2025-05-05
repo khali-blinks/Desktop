ece38f679332babfdcfde7663299a66478042ef0
Khalifa <kismaiel61@gmail.com>
2025-05-05 17:37:29 -0400

    loader

---
+++ Modern React and Redux/registry/src/pages/details/DetailsPage.tsx
@@ -0,0 +1,7 @@
+import type { DetailsLoaderResult } from "./detailsLoader";
+import { useLoaderData } from "react-router-dom";
+
+export default function DetailsPage(){
+    const { details } = useLoaderData() as DetailsLoaderResult;
+    return <div>{details.name} - {details.description}</div>
+}
