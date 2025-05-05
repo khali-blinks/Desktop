bfb8370f8d24839a674aa02ae0ab47786f475c08
Khalifa <kismaiel61@gmail.com>
2025-05-05 17:41:55 -0400

    JSX Name

--- Modern React and Redux/registry/src/pages/details/DetailsPage.tsx
+++ Modern React and Redux/registry/src/pages/details/DetailsPage.tsx
@@ -3,5 +3,16 @@ import { useLoaderData } from "react-router-dom";
 
 export default function DetailsPage(){
     const { details } = useLoaderData() as DetailsLoaderResult;
-    return <div>{details.name} - {details.description}</div>
+
+    return (
+        <div className="space-y-4">
+            <h1 className="text-3xl font-bold">{details.name}</h1>
+            <div>
+                <h3 className="text-lg font-bold">Description</h3>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    )
 }
